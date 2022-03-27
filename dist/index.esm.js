15aab1e049a9cb258b9ab629f0b944c9a4558508
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-03-27 00:17:49 +0000

    Use rollup to bundle package

---
+++ dist/index.esm.js
@@ -0,0 +1,82 @@
+import _extends from '@babel/runtime/helpers/extends';
+import _objectWithoutProperties from '@babel/runtime/helpers/objectWithoutProperties';
+import * as React from 'react';
+
+var _excluded$2 = ["title", "titleId"];
+
+var SvgHeart = function SvgHeart(_ref) {
+  var title = _ref.title,
+      titleId = _ref.titleId,
+      props = _objectWithoutProperties(_ref, _excluded$2);
+
+  return /*#__PURE__*/React.createElement("svg", _extends({
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 471.701 471.701",
+    style: {
+      enableBackground: "new 0 0 471.701 471.701"
+    },
+    xmlSpace: "preserve",
+    width: "1em",
+    height: "1em",
+    "aria-labelledby": titleId
+  }, props), title ? /*#__PURE__*/React.createElement("title", {
+    id: titleId
+  }, title) : null, /*#__PURE__*/React.createElement("path", {
+    d: "M433.601 67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7 13.6-92.4 38.3l-12.9 12.9-13.1-13.1c-24.7-24.7-57.6-38.4-92.5-38.4-34.8 0-67.6 13.6-92.2 38.2-24.7 24.7-38.3 57.5-38.2 92.4 0 34.9 13.7 67.6 38.4 92.3l187.8 187.8c2.6 2.6 6.1 4 9.5 4 3.4 0 6.9-1.3 9.5-3.9l188.2-187.5c24.7-24.7 38.3-57.5 38.3-92.4.1-34.9-13.4-67.7-38.1-92.4zm-19.2 165.7-178.7 178-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3s10.7-53.7 30.3-73.2c19.5-19.5 45.5-30.3 73.1-30.3 27.7 0 53.8 10.8 73.4 30.4l22.6 22.6c5.3 5.3 13.8 5.3 19.1 0l22.4-22.4c19.6-19.6 45.7-30.4 73.3-30.4 27.6 0 53.6 10.8 73.2 30.3 19.6 19.6 30.3 45.6 30.3 73.3.1 27.7-10.7 53.7-30.3 73.3z"
+  }));
+};
+
+var _excluded$1 = ["title", "titleId"];
+
+var SvgLike = function SvgLike(_ref) {
+  var title = _ref.title,
+      titleId = _ref.titleId,
+      props = _objectWithoutProperties(_ref, _excluded$1);
+
+  return /*#__PURE__*/React.createElement("svg", _extends({
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 485.027 485.027",
+    style: {
+      enableBackground: "new 0 0 485.027 485.027"
+    },
+    xmlSpace: "preserve",
+    width: "1em",
+    height: "1em",
+    "aria-labelledby": titleId
+  }, props), title ? /*#__PURE__*/React.createElement("title", {
+    id: titleId
+  }, title) : null, /*#__PURE__*/React.createElement("path", {
+    style: {
+      fill: "#3c92ca"
+    },
+    d: "M109.4 459.991H13c-2.1 0-3.9-1.8-3.9-3.9v-248.4c0-2.1 1.8-3.9 3.9-3.9h96.4c2.1 0 3.9 1.8 3.9 3.9v248.4c0 2.1-1.7 3.9-3.9 3.9z"
+  }), /*#__PURE__*/React.createElement("path", {
+    d: "M284.6 20.391c-28.2-20.7-67.2-8.8-68.8-8.3-3.8 1.2-6.3 4.7-6.3 8.6v85.8c0 29.1-13.8 53.7-41.1 73.2-21.1 15.1-42.7 21.3-42.9 21.4-.2 0-.3.1-.5.2l-5.1 1.7c-3-4.9-8.3-8.2-14.5-8.2H16.9c-9.3 0-16.9 7.6-16.9 16.9v240.5c0 9.3 7.6 16.9 16.9 16.9h88.6c8 0 14.7-5.6 16.4-13 11.9 12.7 28.8 20.7 47.6 20.7h209.8c44.6 0 73.1-23.3 78.1-64l26.8-170.2c3.9-24.7-6.2-49.7-25.8-63.7-11.1-8-24.2-12.2-37.9-12.2H311.4v-79.6c0-31.2-9-53.6-26.8-66.7zm-180.4 430.5H18.1v-238h86.1v238zm316.3-266.1c9.9 0 19.3 3 27.3 8.8 14 10.1 21.3 28.2 18.4 46.2l-26.7 170.3v.3c-4.9 39.8-35.4 48.2-60.2 48.2H169.5c-26 0-47.1-21.1-47.1-47.1v-190.2l8.3-2.8c2.9-.8 25.2-7.6 47.8-23.7 32.1-22.8 49.1-53.3 49.1-88.2v-78.6c10.4-2 31.3-4 46.4 7.1 12.8 9.4 19.3 26.9 19.3 52v88.7c0 5 4.1 9.1 9.1 9.1h118.1v-.1z"
+  }));
+};
+
+var _excluded = ["title", "titleId"];
+
+var SvgShare = function SvgShare(_ref) {
+  var title = _ref.title,
+      titleId = _ref.titleId,
+      props = _objectWithoutProperties(_ref, _excluded);
+
+  return /*#__PURE__*/React.createElement("svg", _extends({
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 465 465",
+    style: {
+      enableBackground: "new 0 0 465 465"
+    },
+    xmlSpace: "preserve",
+    width: "1em",
+    height: "1em",
+    "aria-labelledby": titleId
+  }, props), title ? /*#__PURE__*/React.createElement("title", {
+    id: titleId
+  }, title) : null, /*#__PURE__*/React.createElement("path", {
+    d: "M370.656 290.363c-30.818 0-57.958 16.049-73.502 40.227l-122.311-64.265c4.39-10.408 6.819-21.838 6.819-33.824s-2.429-23.416-6.819-33.824l122.311-64.265c15.545 24.178 42.684 40.227 73.502 40.227 48.148 0 87.319-39.171 87.319-87.318C457.975 39.171 418.804 0 370.656 0c-48.147 0-87.318 39.171-87.318 87.319 0 11.987 2.429 23.416 6.819 33.824l-122.311 64.265c-15.545-24.178-42.684-40.227-73.502-40.227-48.147 0-87.318 39.171-87.318 87.319s39.171 87.319 87.318 87.319c30.818 0 57.958-16.049 73.502-40.227l122.311 64.265c-4.39 10.408-6.819 21.838-6.819 33.824 0 48.148 39.171 87.319 87.318 87.319 48.148 0 87.319-39.171 87.319-87.319 0-48.147-39.171-87.318-87.319-87.318zm0-275.363c39.877 0 72.319 32.442 72.319 72.319 0 39.876-32.442 72.318-72.319 72.318s-72.318-32.442-72.318-72.318C298.337 47.442 330.779 15 370.656 15zM94.343 304.819c-39.876 0-72.318-32.442-72.318-72.319s32.442-72.319 72.318-72.319 72.318 32.442 72.318 72.319-32.442 72.319-72.318 72.319zM370.656 450c-39.877 0-72.318-32.442-72.318-72.319 0-39.876 32.441-72.318 72.318-72.318s72.319 32.442 72.319 72.318c0 39.877-32.442 72.319-72.319 72.319z"
+  }));
+};
+
+export { SvgHeart as Heart, SvgLike as Like, SvgShare as Share };
